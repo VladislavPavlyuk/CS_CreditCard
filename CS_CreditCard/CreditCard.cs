9bed98a4c1886d807ae0e531fd723490c4e65bf3
vladp_d3fqm45 <vladp_d3fqm45@DESKTOP-FQLK9B1>
2023-06-04 13:25:18 +0200

    Add project files.

---
+++ CS_CreditCard/CreditCard.cs
@@ -0,0 +1,123 @@
+﻿
+using Delegate;
+
+namespace CS_CreditCard
+{
+    class CreditCard
+    {
+        readonly string _creditCardNumber;
+
+        string _FullName;
+        string _dateOfExpire;
+        string _pin;
+        double _creditLimit;
+        double _amount;
+
+        public CreditCard(string creditCardNumber,
+                        string fullName,
+                        string dateOfExpire,
+                        string pin,
+                        double amount, 
+                        double creditLimit)
+            {
+                _creditCardNumber = creditCardNumber;
+                _FullName = fullName;
+                _dateOfExpire = dateOfExpire;
+                _pin = pin;
+                _creditLimit = creditLimit;
+                _amount = amount;            
+            }
+
+        public double CurrentAmount
+        {
+            get { return _amount; }
+        }
+
+        public void Put(double amount)
+        {
+            if (EnterPIN())
+            {
+                _amount += amount;
+                del?.Invoke("Сумма " + amount.ToString() + " зачислена на кредитную карту." + " Остаток : " + CurrentAmount + " Кредитный лимит : " + CreditLimit);
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (EnterPIN())
+            {
+                if (amount <= _amount + CreditLimit)
+                {
+                    _amount -= amount;
+
+                    del?.Invoke("Сумма " + amount.ToString() + " снята с кредитной карты." + " Остаток : " + CurrentAmount + " Кредитный лимит : " + CreditLimit);
+                }
+                else
+                {
+                    del?.Invoke("Кредитный лимит " + CreditLimit + " исчерпан." + " Остаток : " + CurrentAmount);
+                }
+            }
+        }
+
+        public double CreditLimit
+        {
+            get { return _creditLimit; }
+        }
+
+        public string Pin
+        {            
+            get => _pin;
+
+            set => _pin = value;
+        }
+        public bool ChangePIN()
+        {
+            Console.WriteLine("\nИзменить PIN? ([y]es/[n]o) ");
+
+            string answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Pin = null;   
+                return true;
+            }
+
+            if (answer == "n") return false;
+              else
+                {
+                    Console.WriteLine("\nНе понял\n");
+                    ChangePIN();
+                    return true;
+                }
+        }
+        public bool EnterPIN()
+        {
+            Console.WriteLine("\nВведите PIN код: ");
+
+            string temp = Console.ReadLine();
+
+            if ((temp.Length == 4) && (Pin == null))
+            {
+                Pin = temp;
+
+                Console.WriteLine("\nPIN код сохранен");
+                
+                if (ChangePIN()) EnterPIN();
+                return true;
+            }
+            if ((temp.Length == 4) && (Pin == temp))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nPIN код введен не верно ");
+
+                EnterPIN();
+            }
+            return false;
+        }
+
+        public event CreditCardStateHandler del;
+    }
+}
---
+++ CS_CreditCard/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+    Делегаты и события
+Создайте класс «Кредитная карточка».
+Класс должен содержать:
+• Номер карты;
+• ФИО владельца;
+• Срок действия карты;
+• PIN;
+• Кредитный лимит;
+• Сумма денег.
+Создайте необходимый набор методов класса.
+Реализуйте события для следующих ситуаций:
+• Пополнение счёта;
+• Расход денег со счёта;
+• Старт использования кредитных денег;
+• Достижение заданной суммы денег;
+• Смена PIN.
+В клиентской части программы продемонстрируйте работу с классом «Кредитная карточка».
+ */
+
+using CS_CreditCard;
+using System;
+
+namespace Delegate
+{
+ 
+    public delegate void CreditCardStateHandler(string message);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard card = new CreditCard("1111 2222 3333 4444", "Vladislav Pavlyuk", "01/01/2024", null, 0, 50);
+
+            card.del += Color_Message;
+
+            card.Put(150);
+
+            card.Withdraw(100);
+
+            card.Put(50);
+
+            card.Withdraw(150);
+
+            card.Withdraw(50);
+
+            card.Put(150);
+        }
+
+        private static void Color_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
+
